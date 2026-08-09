ccc1d44fea645ad71a640899ae38e4e1e323baa1
MikaDevelops <mika.virtala@gmail.com>
2026-08-09 09:54:57 +0300

    some more scripts

--- script.js
+++ script.js
@@ -17,13 +17,10 @@ main_canvas.width =     canvasProperties.width;
 main_canvas.height =    canvasProperties.height;
 
 const ctx = main_canvas.getContext('2d');
-ctx.fillStyle = "gray";
-ctx.fillRect(0,0, main_canvas.width, main_canvas.height);
+clearCanvas()
 
-ctx.fillStyle = "white";
-ctx.fillRect(stageProperties.left, stageProperties.top, stageProperties.width, stageProperties.height);
-
-const path = [];
+let path = [];
+const shapes = [];
 
 main_canvas.addEventListener('pointerdown', function(e){
     const mousePos = {
@@ -31,21 +28,43 @@ main_canvas.addEventListener('pointerdown', function(e){
         y: e.offsetY
     };
     path.push(mousePos);
-});
 
-main_canvas.addEventListener('pointermove', function(e){
-    const mousePos = {
+    const moveCallback = function(e){
+        const mousePos = {
         x: e.offsetX,
         y: e.offsetY
-    };
-    path.push(mousePos);
+        };
+        path.push(mousePos);
+
+        clearCanvas();
+        for (const shape of [...shapes, path]){
+            ctx.beginPath();
+            ctx.moveTo(shape[0].x, shape[0].y);
+            for (let i = 1; i<shape.length;i++){
+                ctx.lineTo(shape[i].x, shape[i].y)
+            }
+            ctx.stroke();
+        }
+    }
+
+    main_canvas.addEventListener('pointermove', moveCallback);
+
+    const upCallback = function(e){
+        main_canvas.removeEventListener('pointermove', moveCallback);
+        main_canvas.removeEventListener('pointerup', upCallback);
+        shapes.push(path);
+        path = [];
+    }
+
+    main_canvas.addEventListener('pointerup', upCallback);
+
 });
 
-main_canvas.addEventListener('pointerup', function(e){
-    ctx.beginPath();
-    ctx.moveTo(path[0].x, path[0].y);
-    path.forEach( (point)=>{
-        ctx.lineTo(point.x, point.y);
-    });
-    ctx.stroke();//45:24
-});
+function clearCanvas(){
+    ctx.clearRect(0,0, main_canvas.width, main_canvas.height);
+    ctx.fillStyle = "gray";
+    ctx.fillRect(0,0, main_canvas.width, main_canvas.height);
+    ctx.fillStyle = "white";
+    ctx.fillRect(stageProperties.left, stageProperties.top, stageProperties.width, stageProperties.height);
+    }
+
